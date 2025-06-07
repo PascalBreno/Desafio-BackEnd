ad0f0afdd0f3d29cfa5de101976d3ab3d60dd622
Breno Felix <brenodml@live.com>
2025-06-07 18:44:32 -0300

    Cria script para a criação das tabelas

--- MotosAluguel.Domain/Entities/Rentals/Rental.cs
+++ MotosAluguel.Domain/Entities/Rentals/Rental.cs
@@ -1,13 +1,24 @@
 ﻿using MotosAluguel.Domain.Entities.Base;
+using MotosAluguel.Domain.Entities.Entregadores;
+using MotosAluguel.Domain.Entities.Motos;
 
 namespace MotosAluguel.Domain.Entities.Locacoes;
 
 public sealed class Rental : EntityBase
 {
     public string RiderId { get; init; }
+
+    public Rider Rider { get; set; }
+
     public string MotorCycleId { get; init; }
+
+    public Motorcycle MotorCycle { get; set; }
+
     public DateTime BeginAt { get; init; }
+
     public DateTime EndAt { get; init; }
+
     public DateTime EstimatedEndDate { get; init; }
+
     public int Plan { get; set; }
 }
---
+++ MotosAluguel.Infra/Scripts/create_table.sql
@@ -0,0 +1,46 @@
+﻿-- Cria tabela para os entregadores
+
+CREATE TABLE Riders (
+    Id VARCHAR(50) PRIMARY KEY,
+    CNPJ VARCHAR(18) NOT NULL UNIQUE,
+    "Name" VARCHAR(100) NOT NULL,
+    CNH VARCHAR(20) NOT NULL UNIQUE,
+    CnhType VARCHAR(50) NOT NULL,
+    BirthDate TIMESTAMP NOT NULL,
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UpdatedAt TIMESTAMP NULL,
+    DeletedAt TIMESTAMP NULL,
+    IsDeleted BOOLEAN DEFAULT FALSE
+);
+
+-- Cria tabela para as motos
+CREATE TABLE MotorCycles(
+    Id VARCHAR(50) PRIMARY KEY,
+    "Year" VARCHAR(18) NOT NULL UNIQUE,
+    Model VARCHAR(100) NOT NULL,
+    Plate VARCHAR(20) NOT NULL UNIQUE,
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UpdatedAt TIMESTAMP NULL,
+    DeletedAt TIMESTAMP NULL,
+    IsDeleted BOOLEAN DEFAULT FALSE
+);
+
+-- Cria tabela para a locação
+CREATE TABLE Rentals (
+    Id UUID PRIMARY KEY,      
+    RiderId VARCHAR(50) NOT NULL,    -- Chave estrangeira para a tabela Riders
+    MotorCycleId VARCHAR(50) NOT NULL, -- Chave estrangeira para a tabela Motorcycles
+    BeginAt TIMESTAMP NOT NULL,      
+    EndAt TIMESTAMP NULL,            
+    EstimatedEndDate TIMESTAMP NOT NULL, 
+    "Plan" INT NOT NULL,               
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    UpdatedAt TIMESTAMP NULL,
+    DeletedAt TIMESTAMP NULL,
+    IsDeleted BOOLEAN DEFAULT FALSE,
+
+    CONSTRAINT fk_rider FOREIGN KEY (RiderId) REFERENCES Riders(Id),
+    CONSTRAINT fk_motorcycle FOREIGN KEY (MotorCycleId) REFERENCES Motorcycles(Id)
+);
+
+
