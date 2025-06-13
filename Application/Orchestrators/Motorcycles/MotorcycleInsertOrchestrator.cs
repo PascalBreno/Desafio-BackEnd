f8d89ea7ac29f5dd3ef56794ef78d9ebde787233
Breno Felix <brenodml@live.com>
2025-06-12 23:56:12 -0300

    Adiciona logs iniciais

--- Application/Orchestrators/Motorcycles/MotorcycleInsertOrchestrator.cs
+++ Application/Orchestrators/Motorcycles/MotorcycleInsertOrchestrator.cs
@@ -3,18 +3,19 @@ using MotosAluguel.Application.Commands.Motorcycles;
 using MotosAluguel.Application.Commons.Response.Motorcycles;
 using MotosAluguel.Application.Interfaces.Orchestrators.Motorcycles;
 using MotosAluguel.Application.Mappers.Motorcycles;
-using MotosAluguel.Domain.Entities.MotorCycles;
 using MotosAluguel.Domain.Interfaces.Repositories.Motorcyles;
 using MotosAluguel.Domain.Interfaces.Validators.Motorcycles;
 using MotosAluguel.Domain.Messaging.Events;
 using MotosAluguel.Domain.Validators.Base;
+using Serilog;
 
 namespace MotosAluguel.Application.Orchestrators.Motorcycles;
 
 public class MotorcycleInsertOrchestrator(
     IMotorcyclesInsertValidator validator,
     IMotorcycleWriterRepository repository,
-    IPublishEndpoint publishEndpoint) : IMotorcycleInsertOrchestrator
+    IPublishEndpoint publishEndpoint,
+    ILogger logger) : IMotorcycleInsertOrchestrator
 {
     private readonly IMotorcyclesInsertValidator _validator = validator;
 
@@ -22,8 +23,12 @@ public class MotorcycleInsertOrchestrator(
 
     private readonly IPublishEndpoint _publishEndpoint = publishEndpoint;
 
+    private readonly ILogger _logger = logger;
+
     public async Task<OperationResult<MotorcycleResponse>> RunAsync(MotorcycleInsertCommand command)
     {
+        _logger.Information("Iiciando o processo de inserção de motocicleta com os dados: {@Command}", command);
+
         var entity = MotorcycleMapper.ToEntity(command);
 
         var operationResult = await _validator.ValidateAsync(entity);
@@ -43,6 +48,8 @@ public class MotorcycleInsertOrchestrator(
             await _publishEndpoint.Publish(registeredEvent);
 
             var response = MotorcycleResponseMapper.ToResponse(entity);
+
+            _logger.Information("Moto inserida com sucesso: {@Response}", response);
 
             return OperationResult<MotorcycleResponse>.Ok(response);
         }
--- MotosAluguel.Infra/Repositories/Rentals/RentalWriterRepositoryWithErrorHandler.cs
+++ MotosAluguel.Infra/Repositories/Rentals/RentalWriterRepositoryWithErrorHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MotosAluguel.Domain.Entities.Rentals;
 using MotosAluguel.Domain.Interfaces.Repositories.Rentals;
+using Serilog;
 
 namespace MotosAluguel.Infra.Repositories.Rentals;
 
@@ -7,19 +8,32 @@ public class RentalWriterRepositoryWithErrorHandler : IRentalWriterRepository
 {
     private readonly IRentalWriterRepository _rentalWriterRepository;
 
-    public RentalWriterRepositoryWithErrorHandler(IRentalWriterRepository rentalWriterRepository)
+    private readonly ILogger _logger;
+
+    public RentalWriterRepositoryWithErrorHandler(
+        IRentalWriterRepository rentalWriterRepository,
+        ILogger logger)
     {
         _rentalWriterRepository = rentalWriterRepository;
+        _logger = logger;
     }
 
     public async Task<Guid> InsertAsync(Rental rental)
     {
         try
         {
-            return await _rentalWriterRepository.InsertAsync(rental);
+            _logger.Information("Iniciando o processo de inserção de aluguel com os dados: {@Rental}", rental);
+
+            var result =  await _rentalWriterRepository.InsertAsync(rental);
+
+            _logger.Information("Aluguel inserido com sucesso: {@RentalId}", result);
+
+            return result;
         }
         catch (Exception ex)
         {
+            _logger.Error(ex, "Erro ao inserir aluguel: {@Rental}", rental);
+
             throw new Exception("An error occurred while inserting the rental", ex);
         }
     }
